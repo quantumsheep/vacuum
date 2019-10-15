94f537a079d9a8f07ecaacfd0f5374d79e07f725
quantumsheep <nathanael.dmc@outlook.fr>
2019-10-15 15:46:56 +0200

    Add dependencies installation script

---
+++ install-dep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    apt-get install libcurl4-openssl-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # brew install curl-openssl
+fi

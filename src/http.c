e79aee2e5d462e7e79a016751324a176a9387f72
quantumsheep <nathanael.dmc@outlook.fr>
2019-10-16 17:00:47 +0200

    Fix http_get

--- src/http.c
+++ src/http.c
@@ -1,13 +1,47 @@
 #include "http.h"
 
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
-const char *http_get(const char *url)
+struct buffer_t
+{
+    char *bytes;
+    size_t len;
+    size_t capacity;
+};
+
+static struct buffer_t init_buffer()
+{
+    struct buffer_t buffer;
+
+    buffer.bytes = calloc(sizeof(char), 0 + 1);
+    buffer.len = 0;
+
+    return buffer;
+}
+
+size_t write_buffer(void *ptr, size_t size, size_t nmemb, struct buffer_t *buffer)
+{
+    size_t nbytes = size * nmemb;
+
+    buffer->bytes = realloc(buffer->bytes, sizeof(char) * (buffer->len + nbytes + 1));
+
+    memcpy(buffer->bytes + buffer->len, ptr, nbytes);
+    buffer->len += nbytes;
+
+    return nbytes;
+}
+
+char *http_get(const char *url)
 {
     CURL *curl = curl_easy_init();
+    struct buffer_t buffer = init_buffer();
 
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_buffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
 
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK)
@@ -16,4 +50,6 @@ const char *http_get(const char *url)
     }
 
     curl_easy_cleanup(curl);
+
+    return buffer.bytes;
 }
--- src/http.h
+++ src/http.h
@@ -1,6 +1,6 @@
 #ifndef HTTP_H
 #define HTTP_H
 
-const char *http_get(const char *url);
+char *http_get(const char *url);
 
 #endif

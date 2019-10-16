afba9648ca15d93d122b3b9514020e4ea6bb41c3
quantumsheep <nathanael.dmc@outlook.fr>
2019-10-17 00:21:27 +0200

    Remove tests in config_load

--- src/config/load.c
+++ src/config/load.c
@@ -5,12 +5,5 @@
 Config config_load(const char *path)
 {
     Vector *tokens = config_tokenize(path);
-
-    for (int i = 0; i < tokens->length; i++)
-    {
-        ConfigToken *token = (ConfigToken *)vector_get(tokens, i);
-        printf("line %d - type %d: '%s'\n", token->line, token->type, token->value);
-    }
-
     return config_parse(tokens);
 }

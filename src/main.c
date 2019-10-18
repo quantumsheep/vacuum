255400111a6cce4500eb566c7dc3214c974f6368
quantumsheep <nathanael.dmc@outlook.fr>
2019-10-19 00:42:34 +0200

    Set max_depth to 3 for testing

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main()
         return 1;
     }
 
-    int max_depth = 5;
+    int max_depth = 3;
     Vector *visited = crawl("https://qtmsheep.com", max_depth, NULL);
 
     for (int i = 0; i < visited->length; i++)

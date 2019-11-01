9fbbc1a991612a6956fede02bd7bbc8231392978
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-01 19:09:07 +0100

    Schedule tasks

--- src/main.c
+++ src/main.c
@@ -2,28 +2,92 @@
 #include "crawl.h"
 #include "datatypes/vector.h"
 #include "file.h"
+#include "scheduler.h"
 
 #include <stdio.h>
+
+static Config config;
+static Vector *workers = NULL;
+
+void run_action(const char *name)
+{
+    MapNode *node = map_get(config.actions, name);
+
+    if (node == NULL)
+        return;
+
+    ConfigAction *action = (ConfigAction *)node->value;
+    char *max_depth_str = map_get_string(action->options, "max-depth");
+
+    int max_depth = (max_depth_str != NULL) ? atoi(max_depth_str) : 1;
+
+    Vector *visited = crawl(action->url, max_depth, NULL);
+    vector_free(visited, VECTOR_FREE_REFERENCE);
+}
+
+void on_exit()
+{
+    for (size_t i = 0; i < workers->length; i++)
+    {
+        Worker *worker = (Worker *)vector_get(workers, i);
+        worker_stop(worker);
+    }
+}
 
 int main()
 {
     int has_error = 0;
-    Config config = config_load("config.sconf", &has_error);
+    config = config_load("config.sconf", &has_error);
 
     if (has_error)
     {
         return 1;
     }
 
-    int max_depth = 3;
-    Vector *visited = crawl("https://qtmsheep.com", max_depth, NULL);
+    /**
+     * Launch all the actions one by one
+     */
+    // for (MapNode *node = config.actions->first; node != NULL; node = node->next)
+    // {
+    //     run_action(node->key);
+    // }
 
-    for (int i = 0; i < visited->length; i++)
+    workers = vector_init();
+    signal(SIGINT, on_exit);
+
+    /**
+     * Start all the tasks
+     */
+    for (MapNode *node = config.tasks->first; node != NULL; node = node->next)
     {
-        printf("LES URLS = %s\n", vector_get_string(visited, i));
+        ConfigTask *task = (ConfigTask *)node->value;
+
+        if (task->actions->length == 0)
+            continue;
+
+        Worker *worker = worker_init((task->hours * 3600) + (task->minutes * 60) + task->seconds);
+
+        for (size_t i = 0; i < task->actions->length; i++)
+        {
+            char *action = vector_get(task->actions, i);
+            Job *job = job_init((void *)run_action, action);
+
+            worker_add(worker, job);
+        }
+
+        vector_push_ref(workers, worker);
+        worker_run(worker);
     }
 
-    vector_free(visited, VECTOR_FREE_REFERENCE);
+    for (size_t i = 0; i < workers->length; i++)
+    {
+        Worker *worker = (Worker *)vector_get(workers, i);
+        worker_wait(worker);
+        worker_free(worker);
+    }
+
+    vector_free(workers, VECTOR_KEEP_REFERENCE);
+
     config_free(config);
 
     return 0;

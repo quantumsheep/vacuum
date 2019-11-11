1d854ffe823143d8efc384812f4204c3117dfad2
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-11 17:52:06 +0100

    Add log functions

---
+++ src/log.c
@@ -0,0 +1,24 @@
+#include "log.h"
+
+#include "parallel/file_writing.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+static const char *log_path = NULL;
+
+void log_init(const char *path)
+{
+    log_path = path;
+}
+
+void log_print(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    vprintf(fmt, args);
+    parallel_file_vwrite(log_path, "a", fmt, args);
+
+    va_end(args);
+}
---
+++ src/log.h
@@ -0,0 +1,8 @@
+#ifndef LOG_H
+#define LOG_H
+
+void log_init(const char *path);
+
+void log_print(const char *fmt, ...);
+
+#endif

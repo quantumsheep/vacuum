9855e399a12e35908131ead0504eb89cee66e34c
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-11 18:55:10 +0100

    Add log_print_timed

--- src/log.c
+++ src/log.c
@@ -4,6 +4,9 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 static const char *log_path = NULL;
 
@@ -20,4 +23,28 @@ void log_print(const char *fmt, ...)
     parallel_file_vwrite(log_path, "a", fmt, args);
 
     va_end(args);
+}
+
+void log_print_timed(const char *fmt, ...)
+{
+    time_t timer;
+    char formated_date[26];
+    struct tm *tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(formated_date, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+
+    char *new_fmt = (char *)calloc(sizeof(char), sizeof(formated_date) + strlen(fmt) + 1 + 3);
+    sprintf(new_fmt, "[%s] %s", formated_date, fmt);
+
+    va_list args;
+    va_start(args, fmt);
+
+    parallel_file_vwrite(log_path, "a", new_fmt, args);
+
+    va_end(args);
+
+    free(new_fmt);
 }
--- src/log.h
+++ src/log.h
@@ -4,5 +4,6 @@
 void log_init(const char *path);
 
 void log_print(const char *fmt, ...);
+void log_print_timed(const char *fmt, ...);
 
 #endif

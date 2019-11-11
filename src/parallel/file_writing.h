19b1335554bf562f9693043ddf14ffab6cfe9232
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-11 16:39:53 +0100

    PARALLEL_FILE_WRITING -> PARALLEL_FILE_WRITING_H

--- src/parallel/file_writing.h
+++ src/parallel/file_writing.h
@@ -1,5 +1,5 @@
-#ifndef PARALLEL_FILE_WRITING
-#define PARALLEL_FILE_WRITING
+#ifndef PARALLEL_FILE_WRITING_H
+#define PARALLEL_FILE_WRITING_H
 
 int parallel_file_append(const char *path, const char *data);
 int parallel_file_write(const char *path, const char *data);

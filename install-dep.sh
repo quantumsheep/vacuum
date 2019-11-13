b57f6c8a94c7a83212aada66b5c7140ad8b4ba5c
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-13 22:19:52 +0100

    Fix install-dep.sh

--- install-dep.sh
+++ install-dep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
-    exit
-fi
-
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root."
+        exit
+    fi
+
     apt-get install libcurl4-openssl-dev
     apt-get install cmake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # brew install curl-openssl
+    brew install curl-openssl
 fi
